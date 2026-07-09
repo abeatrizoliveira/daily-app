b4db8e343d498db7f5722c881caf7bf1b04adff9
Beatriz Oliveira <beatriz.oliveira260607@gmail.com>
2026-07-09 18:24:23 -0300

    feat(task): adiciona flatlist
     Iniciei o desenvolvimento da visualizaçao das tarefas. Já podem ser vistas, mas não estão clicáveis e falta algumas estilizações.

--- src/app/(tabs)/tasks.tsx
+++ src/app/(tabs)/tasks.tsx
@@ -1,17 +1,34 @@
-import { StyleSheet, Modal, View, Pressable } from "react-native";
+import {
+  StyleSheet,
+  Modal,
+  View,
+  Pressable,
+  FlatList,
+  Text,
+} from "react-native";
 import Task from "@features/tasks/task";
 import { useState, useEffect } from "react";
-import { Plus } from "lucide-react-native";
+import { Plus, Filter, Calendar } from "lucide-react-native";
 import { useTheme } from "../../context/themeContext";
 import { supabase } from "@utils/supabase";
+import GlobalStyle from "@themes/global-style";
 
 export default function tasks() {
   const [openTask, isOpenTask] = useState<boolean>(false);
   const [idTask, isIdTask] = useState<number | null>();
   const [userId, setUserId] = useState<any>();
+
+  interface Tarefa {
+    id_tarefa: number;
+    titulo: string;
+    data_tarefa: Date;
+  }
+
+  const [dataTask, setDataTask] = useState<Tarefa[]>();
   const { theme } = useTheme();
 
   const style = CreateStyle(theme);
+  const global = GlobalStyle(theme);
 
   function handleTask() {
     isOpenTask(true);
@@ -30,7 +47,7 @@ export default function tasks() {
       }
     }
     getUser();
-  });
+  }, []);
 
   useEffect(() => {
     async function getTarefa() {
@@ -38,25 +55,97 @@ export default function tasks() {
         const { data, error } = await supabase
           .from("tarefa")
           .select("*")
-          .eq("id_usuario", userId)
+          .eq("id_usuario", userId);
         if (error) console.log(error);
         else {
           if (data && data.length > 0) {
             isIdTask(data[0].id_tarefa);
+            setDataTask(data);
           }
         }
       }
     }
     getTarefa();
-  });
+  }, [userId]);
 
-  console.log(idTask)
+  console.log(idTask);
 
   return (
     <View style={style.container}>
-      <Pressable onPress={handleTask}>
-        <Plus></Plus>
-      </Pressable>
+      <View style={style.buttons}>
+        <Pressable
+          onPress={handleTask}
+          style={({ pressed }) => [
+            global.pressable,
+            style.button,
+            {
+              backgroundColor: pressed
+                ? theme.colors.background == "#fff"
+                  ? theme.colors.secundaryAlt
+                  : theme.colors.primaryAlt
+                : theme.colors.background == "#fff"
+                  ? theme.colors.secundary
+                  : theme.colors.primary,
+              transform: [{ scale: pressed ? 0.95 : 1 }],
+              width: 45,
+              height: 45,
+            },
+          ]}
+        >
+          <Plus color={theme.colors.background} />
+        </Pressable>
+        <View style={style.minorButtons}>
+          <Pressable
+            onPress={handleTask}
+            style={({ pressed }) => [
+              global.pressable,
+              style.button,
+              {
+                backgroundColor: pressed
+                  ? theme.colors.background == "#fff"
+                    ? theme.colors.secundaryAlt
+                    : theme.colors.primaryAlt
+                  : theme.colors.background == "#fff"
+                    ? theme.colors.secundary
+                    : theme.colors.primary,
+                transform: [{ scale: pressed ? 0.95 : 1 }],
+              },
+            ]}
+          >
+            <Calendar color={theme.colors.background} />
+          </Pressable>
+          <Pressable
+            onPress={handleTask}
+            style={({ pressed }) => [
+              global.pressable,
+              style.button,
+              {
+                backgroundColor: pressed
+                  ? theme.colors.background == "#fff"
+                    ? theme.colors.secundaryAlt
+                    : theme.colors.primaryAlt
+                  : theme.colors.background == "#fff"
+                    ? theme.colors.secundary
+                    : theme.colors.primary,
+                transform: [{ scale: pressed ? 0.95 : 1 }],
+              },
+            ]}
+          >
+            <Filter color={theme.colors.background} />
+          </Pressable>
+        </View>
+      </View>
+      <FlatList
+        style={{ width: "100%" }}
+        data={dataTask}
+        keyExtractor={(item) => item.id_tarefa.toString()}
+        renderItem={({ item }) => (
+          <View style={style.TaskContainer}>
+            <Text>{item.titulo}</Text>
+            <Text>{item.data_tarefa}</Text>
+          </View>
+        )}
+      />
       {openTask && (
         <Modal
           animationType="slide"
@@ -81,12 +170,42 @@ const CreateStyle = (theme: any) =>
       position: "relative",
       justifyContent: "center",
       alignItems: "center",
-    },
-    content: {
       padding: 25,
     },
     taskContainer: {
       width: "80%",
       height: 20,
+    },
+    buttons: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+      width: "100%",
+    },
+    minorButtons: {
+      flexDirection: "row",
+      gap: 15,
+    },
+    button: {
+      width: 40,
+      height: 40,
+      borderRadius: 10,
+      backgroundColor:
+        theme.colors.background == "#fff"
+          ? theme.colors.secundary
+          : theme.colors.primary,
+    },
+    TaskContainer: {
+      width: "100%",
+      height: "auto",
+      padding: 15,
+      backgroundColor: theme.colors.backgroundAlt,
+      marginTop: 30,
+      borderRadius: 30,
+      borderWidth: 2,
+      borderColor:
+        theme.colors.background == "#fff"
+          ? theme.colors.secundary
+          : theme.colors.primary,
     },
   });
