2435454f6d7587cc33373f01d7885226cb876f11
Beatriz Oliveira <beatriz.oliveira260607@gmail.com>
2026-08-05 15:24:04 -0300

    develop: adiciona .env
    Configurei o .en
    para garantir a segurança e evitar o vazamento da API_KEY

--- src/features/home/home.service.tsx
+++ src/features/home/home.service.tsx
@@ -1,5 +1,8 @@
-export async function getWeatherApi(city:string) {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=api_key&units=metric&lang=pt_br`;
+import { API_KEY } from "@env";
+
+export async function getWeatherApi(city: string) {
+  const apikey = API_KEY;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric&lang=pt_br`;
   const response = await fetch(url);
   return response.json();
 }
